34dfa73c1c4d1d482c649b3da6df34e441e580ef
Tyler Lindberg <39677147+zveroboy152@users.noreply.github.com>
2022-12-18 22:31:26 -0800

    Update EC2-to-S3-Migrator.sh

--- EC2-to-S3-Migrator.sh
+++ EC2-to-S3-Migrator.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 
-# Set the name of the S3 bucket
-BUCKET_NAME=my-s3-bucket
+#Permissions to set on the S3 bucket taht YOU created:
 
-# Check if the S3 bucket exists, and create it if it doesn't
-if ! aws s3 ls "s3://$BUCKET_NAME" > /dev/null 2>&1; then
-  aws s3 mb "s3://$BUCKET_NAME"
-fi
+#added Grantee, Region-specific canonical account ID to S3 ACL with read/write permission
 
-# Get the IDs of all turned off EC2 instances
-INSTANCE_IDS=$(aws ec2 describe-instances \
-  --filters "Name=instance-state-name,Values=stopped" \
-  --query "Reservations[*].Instances[*].InstanceId" \
-  --output text)
+#Navigate to: Amazon S3 -> Buckets -> yourbucketname -> permissions -> Access control list (ACL) -> Edit
 
-# Iterate over the instance IDs and move each instance to the S3 bucket
-for INSTANCE_ID in $INSTANCE_IDS; do
-  aws s3 cp s3://$BUCKET_NAME/$INSTANCE_ID.tar.gz s3://$BUCKET_NAME/
+#Find section named: Access for other AWS accounts, add relevant canonical account ID and permissions
+
+#After adding canonical account ID: enter image description here
+
+#Find relevent grantee by ref:
+
+#https://docs.aws.amazon.com/vm-import/latest/userguide/vmexport.html
+
+# Set the region to scan for instances
+
+region=us-east-2
+
+# Set the prefix to use for the exported instances in the S3 bucket (the container in the bucket)
+s3_prefix=vms/
+
+# Generate a unique name for the S3 bucket
+bucket_name=
+ 
+# Create the export task configuration file
+cat > export_task_config.json << EOF
+{
+    "ContainerFormat": "ova",
+    "DiskImageFormat": "VMDK",
+    "S3Bucket": "$bucket_name",
+    "S3Prefix": "$s3_prefix"
+}
+EOF
+
+# Get a list of all stopped EC2 instances in the specified region
+instances=$(aws ec2 describe-instances --region $region --filters "Name=instance-state-name,Values=stopped" --query "Reservations[*].Instances[*].[InstanceId]" --output text)
+
+# Loop through the list of instances and export each one to the S3 bucket
+for instance in $instances; do
+  aws ec2 create-instance-export-task --instance-id $instance --target-environment vmware --export-to-s3-task file://export_task_config.json --region $region
 done
 
-# Print the final result
-echo "Successfully moved all turned off EC2 instances to S3 bucket '$BUCKET_NAME'"
+# Clean up the export task configuration file
+rm export_task_config.json
